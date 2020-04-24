b3d60b42e571d77c86444c75181bdf6b42cc850a
First <first@thai.run>
2020-04-24 13:18:00 +0700

    add event detail

--- src/page/EventDetail.js
+++ src/page/EventDetail.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useQuery, useMutation } from 'react-apollo'
-import { Spinner, Table, Button, Modal } from 'react-bootstrap';
+import { Spinner, Table, Button, Modal, Row, Col } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container'
 // import moment from 'moment'
 import AuthContext from '../context/AuthContext'
 import getactivityhasevent from '../graphql/queries/getActivityhasevent'
 import RegisterEvent from '../graphql/mutations/RegisterEvent'
 import _ from 'lodash'
+import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRunning, faClock, faRoad } from '@fortawesome/free-solid-svg-icons'
 
 const EventDetail = props => {
   const { history } = props
   const { eventId } = props.match.params
   const { user } = useContext(AuthContext)
-  const { data = { activityhasevent: []}, loading } = useQuery(getactivityhasevent,{
+  const { data = { activityhasevent: [], eventOne: {}}, loading } = useQuery(getactivityhasevent,{
     variables: {
       eventId: Number(eventId)
     }
@@ -44,10 +47,45 @@ const EventDetail = props => {
       }
     }
   }
+  const calDate = (enddate, startdate) => {
+    const end_date = new Date(enddate)
+    const start_date = new Date(startdate)
+    const diffTime = Math.abs(end_date - start_date)
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    return diffDays
+  }
+  const calDistance = (activities) => {
+    const total = _.sumBy(activities, function(o) {return o.distance})
+    return total
+  }
+  const calTotalDistance = (activitiesevent) => {
+    let total = 0
+    activitiesevent.forEach(element => {
+      total = total + calDistance(element.activities)
+    })
+    return total
+  }
+  if(loading){
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    )
+  }
+  
   return (
     <Container>
       <Button onClick={handleShow} variant="primary" disabled={data.activityhasevent.find(ele => Number(ele._id) === user.id) !== undefined}>สมัคร</Button>
       {console.log(data)}
+      <div>
+      <p>start: {moment(data.eventOne.start_date).format('YYYY/MM/DD')}</p>
+      <p>end: {moment(data.eventOne.end_date).format('YYYY/MM/DD')}</p>
+      </div>
+      <Row>
+        <Col style={{height: '100px'}}><FontAwesomeIcon size="3x" icon={faRunning} /><p>{data.activityhasevent.length} Runner</p></Col>
+        <Col><FontAwesomeIcon size="3x" icon={faClock} /><p>{calDate(data.eventOne.end_date, data.eventOne.start_date)} Day</p></Col>
+        <Col><FontAwesomeIcon size="3x" icon={faRoad} /><p>{(calTotalDistance(data.activityhasevent) / 1000).toFixed(2)} km.</p></Col>
+      </Row>
       <Table>
         <thead>
           <tr  style={{backgroundColor:'#FFA500',color:'#222'}}>
